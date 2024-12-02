004b10cf0f99c7a08e893acc1586424d77a70e07
PanCytryn <pancytryn.dawid@gmail.com>
2024-12-02 12:20:17 +0100

    end

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Dawid Stępniak')
+})
+
+app.get('/test/:num1/:num2', function (req, res) {
+    const num1 = parseInt(req.params.num1)
+    const num2 = parseInt(req.params.num2)
+    const sum = num1 + num2;
+    res.send(sum)
+})
+
+app.listen(80, () => {
+    console.log("OK");
+})
