20aa6b96ef4a40beb8d8e8d537b453d4f22a5919
PanCytryn <pancytryn.dawid@gmail.com>
2024-12-09 09:55:50 +0100

    stringify res.send

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.get('/', function (req, res) {
 app.get('/test/:num1/:num2', function (req, res) {
     const num1 = parseInt(req.params.num1)
     const num2 = parseInt(req.params.num2)
-    const sum = num1 + num2;
-    res.send(sum)
+    res.send(`${num1 + num2}`)
 })
 
 app.listen(80, () => {
